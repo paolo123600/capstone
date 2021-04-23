26b46a051cd376ffbf7c2bf4a828637b4dfee511
paolo123600 <cabrera.paolo.itc2018@gmail.com>
2021-04-24 00:39:24 +0800

    First Commit

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.capstone;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +13,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +35,24 @@ public class MainActivity extends AppCompatActivity {
     public void openActivity2(){
         Intent intent = new Intent(this, burger_test.class);
         startActivity(intent);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.nagiation_drawer_open, R.string.nagiation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 }
