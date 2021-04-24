43cfbd84192d21bc9361b281217f0cc41fbcc2f6
JohnBalagso <jpbalagso16@gmail.com>
2021-04-24 16:34:10 +0800

    commited

---
+++ app/src/main/java/com/example/capstone/Sign_UpActivity.java
@@ -0,0 +1,13 @@
+package com.example.capstone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Sign_UpActivity extends AppCompatActivity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.sign_up);
+
+}}
