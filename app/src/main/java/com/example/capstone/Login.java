82d2f70642b62a7691624431a2234145b0628d6e
paolo123600 <cabrera.paolo.itc2018@gmail.com>
2021-05-12 02:59:35 +0800

    First Commit

--- app/src/main/java/com/example/capstone/Login.java
+++ app/src/main/java/com/example/capstone/Login.java
@@ -102,7 +102,6 @@ public class Login extends AppCompatActivity {
                 }else if (pass.getText().toString().trim().isEmpty()){
                     Toast.makeText(Login.this, "Enter Password",Toast.LENGTH_SHORT).show();
                 }else{
-
                     progressbg.setVisibility(View.VISIBLE);
                     signInProgressBar.setVisibility(View.VISIBLE);
                     userlogin();
