71abb8a792c30769677af0dee5e120d08e9cf710
JohnBalagso <jpbalagso16@gmail.com>
2021-08-08 02:09:22 +0800

    fds

--- app/src/main/java/com/example/capstone/notification.java
+++ app/src/main/java/com/example/capstone/notification.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -40,78 +42,53 @@ FirestoreRecyclerAdapter adapter;
         mFirestorelist = (RecyclerView)findViewById(R.id.recycleview_notif);
         db= FirebaseFirestore.getInstance();
 
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this,
+                R.array.Status, android.R.layout.simple_spinner_item);
+
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner_status.setAdapter(arrayAdapter);
+
         Query query = db.collection("Schedules").orderBy("Dnt", Query.Direction.ASCENDING).limit(20);
 
-        FirestoreRecyclerOptions<DocTodaySchedModel> options = new FirestoreRecyclerOptions.Builder<DocTodaySchedModel>()
-                .setQuery(query, DocTodaySchedModel.class)
-                .build();
-        adapter = new FirestoreRecyclerAdapter<DocTodaySchedModel, Schedholder>(options) {
-            @NonNull
+        Shownotif(query);
+
+        spinner_status.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public Schedholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.notif_single,parent,false);
-                return new Schedholder(view);
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Query query;
+                String selectedstat = spinner_status.getSelectedItem().toString();
+                switch (selectedstat) {
+                    case "All":
+                        query = db.collection("Schedules").orderBy("Dnt", Query.Direction.ASCENDING).limit(20);
+                        Shownotif(query);
+                        break;
+                    case "Booked":
+                         query = db.collection("Schedules").whereEqualTo("Status","Paid").orderBy("Dnt", Query.Direction.ASCENDING).limit(20);
+                        Shownotif(query);
+                        break;
+                    case "Pending":
+                         query = db.collection("Schedules").whereEqualTo("Status","Pending Approval").orderBy("Dnt", Query.Direction.ASCENDING).limit(20);
+                        Shownotif(query);
+                        break;
+                    case "Rescheduled":
+                         query = db.collection("Schedules").whereEqualTo("Status","Rescheduled").orderBy("Dnt", Query.Direction.ASCENDING).limit(20);
+                        Shownotif(query);
+                        break;
+                    case "Cancelled":
+                         query = db.collection("Schedules").whereEqualTo("Status","Cancelled").orderBy("Dnt", Query.Direction.ASCENDING).limit(20);
+                        Shownotif(query);
+                        break;
+                }
+            ;
             }
 
             @Override
-            protected void onBindViewHolder(@NonNull Schedholder holder, int position, @NonNull DocTodaySchedModel model) {
-                String date= model.getDate();
-                String status = model.getStatus();
-                Date bookeddate = model.getDnt();
-                        db.collection("Patients").document(model.getPatientUId()).get()
-                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                            @Override
-                            public void onSuccess(DocumentSnapshot documentSnapshot1) {
-                                documentSnapshot1.getData();
-                                String patname = documentSnapshot1.getString("LastName")+", "+documentSnapshot1.getString("FirstName");
-                                db.collection("Doctors").document(model.getDoctorUId()).get()
-                                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                                            @Override
-                                            public void onSuccess(DocumentSnapshot documentSnapshot2) {
-                                                documentSnapshot2.getData();
-                                                String docname= "Doc. "+documentSnapshot2.getString("LastName");
-                                                switch (status){
-                                                    case "Paid":
-                                                        holder.tvpatname.setText(patname+" has booked an appointment with "+docname+" for "+date);
-                                                        break;
-                                                    case "Pending Approval":
-                                                        holder.tvpatname.setText(patname+" has booked an appointment with "+docname+" that is currently pending for "+date);
-                                                        break;
-                                                    case "Rescheduled":
-                                                        holder.tvpatname.setText(patname+" has rescheduled an appointment with "+docname+" dated "+date);
-                                                        break;
-                                                    case "Cancelled":
-                                                        holder.tvpatname.setText(patname+" has cancelled an appointment with "+docname+" dated "+date);
-                                                        break;
-
-                                                }
-                                                SimpleDateFormat simpleDate =  new SimpleDateFormat("MMM d ,yyyy h:ma");
-                                                String bookeddatestring = simpleDate.format(bookeddate);
-                                                holder.tvdate.setText(bookeddatestring);
-                                            }
-                                        }).addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Toast.makeText(notification.this, "error showing patient", Toast.LENGTH_SHORT).show();
-                                    }
-                                });
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(notification.this, "error showing patient", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-
-
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
-        };
-            mFirestorelist.setHasFixedSize(true);
-            mFirestorelist.setLayoutManager(new LinearLayoutManager(this));
-            mFirestorelist.setAdapter(adapter);
-            adapter.startListening();
+        });
+
     }
 
     private class Schedholder extends RecyclerView.ViewHolder{
@@ -124,9 +101,78 @@ FirestoreRecyclerAdapter adapter;
         }
     }
 
-     public void Shownotif(){
+     public void Shownotif(Query query){
+
+         FirestoreRecyclerOptions<DocTodaySchedModel> options = new FirestoreRecyclerOptions.Builder<DocTodaySchedModel>()
+                 .setQuery(query, DocTodaySchedModel.class)
+                 .build();
+         adapter = new FirestoreRecyclerAdapter<DocTodaySchedModel, Schedholder>(options) {
+             @NonNull
+             @Override
+             public Schedholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                 View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.notif_single,parent,false);
+                 return new Schedholder(view);
+             }
+
+             @Override
+             protected void onBindViewHolder(@NonNull Schedholder holder, int position, @NonNull DocTodaySchedModel model) {
+                 String date= model.getDate();
+                 String status = model.getStatus();
+                 Date bookeddate = model.getDnt();
+                 db.collection("Patients").document(model.getPatientUId()).get()
+                         .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                             @Override
+                             public void onSuccess(DocumentSnapshot documentSnapshot1) {
+                                 documentSnapshot1.getData();
+                                 String patname = documentSnapshot1.getString("LastName")+", "+documentSnapshot1.getString("FirstName");
+                                 db.collection("Doctors").document(model.getDoctorUId()).get()
+                                         .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                             @Override
+                                             public void onSuccess(DocumentSnapshot documentSnapshot2) {
+                                                 documentSnapshot2.getData();
+                                                 String docname= "Doc. "+documentSnapshot2.getString("LastName");
+                                                 switch (status){
+                                                     case "Paid":
+                                                         holder.tvpatname.setText(patname+" has booked an appointment with "+docname+" for "+date);
+                                                         break;
+                                                     case "Pending Approval":
+                                                         holder.tvpatname.setText(patname+" has booked an appointment with "+docname+" that is currently pending for "+date);
+                                                         break;
+                                                     case "Rescheduled":
+                                                         holder.tvpatname.setText(patname+" has rescheduled an appointment with "+docname+" dated "+date);
+                                                         break;
+                                                     case "Cancelled":
+                                                         holder.tvpatname.setText(patname+" has cancelled an appointment with "+docname+" dated "+date);
+                                                         break;
+
+                                                 }
+                                                 SimpleDateFormat simpleDate =  new SimpleDateFormat("MMM d ,yyyy h:ma");
+                                                 String bookeddatestring = simpleDate.format(bookeddate);
+                                                 holder.tvdate.setText(bookeddatestring);
+                                             }
+                                         }).addOnFailureListener(new OnFailureListener() {
+                                     @Override
+                                     public void onFailure(@NonNull Exception e) {
+                                         Toast.makeText(notification.this, "error showing patient", Toast.LENGTH_SHORT).show();
+                                     }
+                                 });
+                             }
+                         }).addOnFailureListener(new OnFailureListener() {
+                     @Override
+                     public void onFailure(@NonNull Exception e) {
+                         Toast.makeText(notification.this, "error showing patient", Toast.LENGTH_SHORT).show();
+                     }
+                 });
 
 
+
+
+             }
+         };
+         mFirestorelist.setHasFixedSize(true);
+         mFirestorelist.setLayoutManager(new LinearLayoutManager(this));
+         mFirestorelist.setAdapter(adapter);
+         adapter.startListening();
 
     }
 }
