cea6c6a2accf27c8f7d926a5804c3d3815005b06
paolo123600 <cabrera.paolo.itc2018@gmail.com>
2021-08-18 00:25:09 +0800

    pao commit

--- app/src/main/java/com/example/capstone/pat_blood_pressure.java
+++ app/src/main/java/com/example/capstone/pat_blood_pressure.java
@@ -1,19 +1,27 @@
 package com.example.capstone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -24,34 +32,39 @@ public class pat_blood_pressure extends AppCompatActivity {
 
     TextView bpdate;
     DatePickerDialog.OnDateSetListener listener;
-    EditText bplog;
+    EditText bpupper;
+    EditText bplower;
     Button submitbp;
 
     FirebaseAuth fAuth;
     FirebaseFirestore db;
     String userId;
-    String collectionBpDate, edittxtbp;
-    int asd;
-    int qwe;
-    int hjk;
+    String collectionBpDate, edittxtupper, edittxtlower;
+    RecyclerView mFirestoreList;
+
+    private FirestoreRecyclerAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pat_blood_pressure);
 
         bpdate = findViewById(R.id.bp_date);
-        bplog = findViewById(R.id.bp_log);
+        bpupper = findViewById(R.id.bp_upper);
+        bplower = findViewById(R.id.bp_lower);
+
         submitbp = findViewById(R.id.submit_bp);
 
-        asd = 1;
-        qwe = 2;
-        hjk = 3;
 
 
+        mFirestoreList = findViewById(R.id.recview_bp);
         fAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
 
+
+
         userId = fAuth.getCurrentUser().getUid();
+
 
         Calendar calendar = Calendar.getInstance();
         final int year = calendar.get(Calendar.YEAR);
@@ -59,6 +72,33 @@ public class pat_blood_pressure extends AppCompatActivity {
         final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
         bpdate.setText(getTodaysDate());
+
+
+        Query query = db.collection("Patients").document(userId).collection("BP").orderBy("Dnt", Query.Direction.DESCENDING).limit(20);;
+
+        FirestoreRecyclerOptions<BPModel> options = new FirestoreRecyclerOptions.Builder<BPModel>().setQuery(query, BPModel.class).build();
+
+
+        adapter = new FirestoreRecyclerAdapter<BPModel, BPViewHolder>(options) {
+            @NonNull
+            @Override
+            public BPViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.bp_list, parent, false);
+                return new BPViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull BPViewHolder holder, int position, @NonNull BPModel model) {
+                holder.list_bpressure.setText("BP: " + model.getUpper() + "/" + model.getLower());
+                holder.list_dnt.setText(model.getDnt() + "");
+
+            }
+        };
+
+        mFirestoreList.setHasFixedSize(true);
+        mFirestoreList.setLayoutManager(new LinearLayoutManager(this));
+        mFirestoreList.setAdapter(adapter);
+
 
 
 
@@ -73,19 +113,26 @@ public class pat_blood_pressure extends AppCompatActivity {
             }
         };
 
+
+
         submitbp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 collectionBpDate = bpdate.getText().toString();
-                edittxtbp = bplog.getText().toString();
+                edittxtupper = bpupper.getText().toString();
+                edittxtlower = bplower.getText().toString();
 
                 Date currentTime = Calendar.getInstance().getTime();
                 Map<String, Object> patbp = new HashMap<>();
                 patbp.put("Date", collectionBpDate);
-                patbp.put("BP", edittxtbp);
+                patbp.put("Upper", edittxtupper);
+                patbp.put("Lower", edittxtlower);
                 patbp.put("Dnt", currentTime);
 
-                db.collection("Patients").document(userId).collection("BP").document(collectionBpDate).set(patbp);
+                String ctime;
+                ctime = currentTime.toString();
+
+                db.collection("Patients").document(userId).collection("BP").document(ctime).set(patbp);
 
             }
         });
@@ -102,5 +149,29 @@ public class pat_blood_pressure extends AppCompatActivity {
         month = month + 1;
         int day = calendar.get(Calendar.DAY_OF_MONTH);
         return makeDateString(day, month, year);
+    }
+
+    private class BPViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView list_bpressure, list_date, list_dnt;
+
+        public BPViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            list_bpressure = itemView.findViewById(R.id.BPlist_bloodp);
+            list_dnt = itemView.findViewById(R.id.BPlist_dnt);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
     }
 }
